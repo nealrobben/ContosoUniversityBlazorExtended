1e453df9dcd5b9ef0c7d46b36c5969c30cfd9d05
Neal Robben <neal.robben@gmail.com>
2021-08-22 23:29:22 +0200

    Added loading indicator to About page

--- ContosoUniversityBlazorExtended/WebUI/Server/Controllers/AboutController.cs
+++ ContosoUniversityBlazorExtended/WebUI/Server/Controllers/AboutController.cs
@@ -1,5 +1,6 @@
 ﻿using ContosoUniversityBlazor.Application.Home.Queries.GetAboutInfo;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading;
 using System.Threading.Tasks;
 using WebUI.Shared.Home.Queries.GetAboutInfo;
 
@@ -10,6 +11,9 @@ namespace ContosoUniversityBlazor.WebUI.Controllers
         [HttpGet]
         public async Task<ActionResult<AboutInfoVM>> GetAboutInfo()
         {
+            //To demo the loading indicator
+            Thread.Sleep(2000);
+
             var result = await Mediator.Send(new GetAboutInfoQuery());
 
             return Ok(result);
