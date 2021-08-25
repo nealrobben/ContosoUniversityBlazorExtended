efe4d5596d1c09e0ec9d130e01425ba5b87c9271
Neal Robben <neal.robben@gmail.com>
2021-08-25 18:04:31 +0200

    Implemented tab + chart on about page

--- ContosoUniversityBlazorExtended/WebUI/Client/Pages/Home/About.razor.cs
+++ ContosoUniversityBlazorExtended/WebUI/Client/Pages/Home/About.razor.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -13,9 +16,16 @@ namespace WebUI.Client.Pages.Home
         [Inject]
         public HttpClient Http { get; set; }
 
+        public List<ChartSeries> Series = new List<ChartSeries>();
+        public string[] XAxisLabels = {};
+
         protected override async Task OnInitializedAsync()
         {
             aboutInfo = await Http.GetFromJsonAsync<AboutInfoVM>("/api/about");
+
+            XAxisLabels = aboutInfo.Items.Select(x => x.EnrollmentDate.Value.ToShortDateString()).ToArray();
+            var chartSeries = new ChartSeries() { Name = "Students", Data = aboutInfo.Items.Select(x => (double)x.StudentCount).ToArray() };
+            Series = new List<ChartSeries> { chartSeries };
         }
     }
 }
