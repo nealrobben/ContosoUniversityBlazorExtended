69aab1de526761e7f5ccacc2a623d4d26610f977
Neal Robben <neal.robben@gmail.com>
2021-09-15 14:16:45 +0200

    Added controller for file uploads

---
+++ ContosoUniversityBlazorExtended/WebUI/Server/Controllers/FilesaveController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using WebUI.Shared;
+
+namespace WebUI.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilesaveController : Controller
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly ILogger<FilesaveController> _logger;
+
+        public FilesaveController(IWebHostEnvironment env, ILogger<FilesaveController> logger)
+        {
+            _env = env;
+            _logger = logger;
+        }
+
+        public async Task<ActionResult<IList<UploadResult>>> UploadFile([FromForm] IEnumerable<IFormFile> files)
+        {
+            const int maxAllowedFiles = 3;
+            const long maxFileSize = 1024 * 1024 * 15;
+            var filesProcessed = 0;
+            var resourcePath = new Uri($"{Request.Scheme}://{Request.Host}/");
+            List<UploadResult> uploadResults = new();
+
+            foreach (var file in files)
+            {
+                var uploadResult = new UploadResult();
+                string trustedFileNameForFileStorage;
+                var untrustedFileName = file.FileName;
+                uploadResult.FileName = untrustedFileName;
+                var trustedFileNameForDisplay =
+                    WebUtility.HtmlEncode(untrustedFileName);
+
+                if (filesProcessed < maxAllowedFiles)
+                {
+                    if (file.Length == 0)
+                    {
+                        _logger.LogInformation("{FileName} length is 0 (Err: 1)",
+                            trustedFileNameForDisplay);
+                    }
+                    else if (file.Length > maxFileSize)
+                    {
+                        _logger.LogInformation("{FileName} of {Length} bytes is " +
+                            "larger than the limit of {Limit} bytes (Err: 2)",
+                            trustedFileNameForDisplay, file.Length, maxFileSize);
+                    }
+                    else
+                    {
+                        try
+                        {
+                            trustedFileNameForFileStorage = (new Guid()).ToString() + ".jpg"; //May not actually be a jpeg, fix later
+                            var path = Path.Combine(_env.WebRootPath, "img", "ProfilePictures",
+                                trustedFileNameForFileStorage);
+
+                            await using FileStream fs = new(path, FileMode.Create);
+                            await file.CopyToAsync(fs);
+
+                            _logger.LogInformation("{FileName} saved at {Path}",
+                                trustedFileNameForDisplay, path);
+                            uploadResult.Uploaded = true;
+                            uploadResult.StoredFileName = trustedFileNameForFileStorage;
+                        }
+                        catch (IOException ex)
+                        {
+                            _logger.LogError("{FileName} error on upload (Err: 3): {Message}",
+                                trustedFileNameForDisplay, ex.Message);
+                            uploadResult.ErrorCode = 3;
+                        }
+                    }
+
+                    filesProcessed++;
+                }
+                else
+                {
+                    _logger.LogInformation("{FileName} not uploaded because the " +
+                        "request exceeded the allowed {Count} of files (Err: 4)",
+                        trustedFileNameForDisplay, maxAllowedFiles);
+                    uploadResult.ErrorCode = 4;
+                }
+
+                uploadResults.Add(uploadResult);
+            }
+
+            return new CreatedResult(resourcePath, uploadResults);
+        }
+    }
+}
---
+++ ContosoUniversityBlazorExtended/WebUI/Shared/UploadResult.cs
@@ -0,0 +1,10 @@
+﻿namespace WebUI.Shared
+{
+    public class UploadResult
+    {
+        public bool Uploaded { get; set; }
+        public string FileName { get; set; }
+        public string StoredFileName { get; set; }
+        public int ErrorCode { get; set; }
+    }
+}
