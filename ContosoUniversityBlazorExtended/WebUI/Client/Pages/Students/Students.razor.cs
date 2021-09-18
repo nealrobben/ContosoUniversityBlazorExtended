88f19451a5962f6ab5f0086eb669a3ca804da6bb
Neal Robben <neal.robben@gmail.com>
2021-09-18 18:01:37 +0200

    WIP adding new students table

--- ContosoUniversityBlazorExtended/WebUI/Client/Pages/Students/Students.razor.cs
+++ ContosoUniversityBlazorExtended/WebUI/Client/Pages/Students/Students.razor.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using WebUI.Client.ViewModels.Students;
+using WebUI.Shared.Students.Queries.GetStudentDetails;
 
 namespace WebUI.Client.Pages.Students
 {
@@ -12,6 +15,11 @@ namespace WebUI.Client.Pages.Students
         protected override async Task OnInitializedAsync()
         {
             await StudentsViewModel.OnInitializedAsync();
+        }
+
+        private async Task<TableData<StudentDetailsVM>> ServerReload(TableState state)
+        {
+            return new TableData<StudentDetailsVM>() { TotalItems = 0, Items = new List<StudentDetailsVM>() };
         }
     }
 }
