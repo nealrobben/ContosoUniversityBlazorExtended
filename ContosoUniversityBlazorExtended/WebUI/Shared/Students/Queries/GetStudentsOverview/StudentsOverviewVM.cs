c345ea37d90a98ba4006da8bffade7468230f2ae
Neal Robben <neal.robben@gmail.com>
2021-09-19 21:49:23 +0200

    Moved MetaData class to separate file

--- ContosoUniversityBlazorExtended/WebUI/Shared/Students/Queries/GetStudentsOverview/StudentsOverviewVM.cs
+++ ContosoUniversityBlazorExtended/WebUI/Shared/Students/Queries/GetStudentsOverview/StudentsOverviewVM.cs
@@ -34,37 +34,4 @@ namespace WebUI.Shared.Students.Queries.GetStudentsOverview
             }
         }
     }
-
-    public class MetaData
-    {
-        public int PageNumber { get; set; }
-
-        public int TotalPages { get; set; }
-
-        public int TotalRecords { get; set; }
-
-        public string CurrentSort { get; set; }
-
-        public string NameSortParm { get; set; }
-
-        public string DateSortParm { get; set; }
-
-        public string CurrentFilter { get; set; }
-
-        public bool HasPreviousPage
-        {
-            get
-            {
-                return (PageNumber > 1);
-            }
-        }
-
-        public bool HasNextPage
-        {
-            get
-            {
-                return (PageNumber < TotalPages);
-            }
-        }
-    }
 }
