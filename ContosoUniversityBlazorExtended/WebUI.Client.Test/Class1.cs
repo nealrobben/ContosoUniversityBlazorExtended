190511552eb59858034282212bdcb5dcc044a8ef
Neal Robben <neal.robben@gmail.com>
2021-11-13 22:43:02 +0100

    Added test project

---
+++ ContosoUniversityBlazorExtended/WebUI.Client.Test/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace WebUI.Client.Test
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
