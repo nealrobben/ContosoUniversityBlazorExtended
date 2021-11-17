91f6fefeedbcc4d8176301d46dd5958013b9e9e3
Neal Robben <neal.robben@gmail.com>
2021-11-17 15:37:28 +0100

    Removed unused code

--- ContosoUniversityBlazorExtended/Infrastructure/DependencyInjection.cs
+++ ContosoUniversityBlazorExtended/Infrastructure/DependencyInjection.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.IO;
 
 namespace ContosoUniversityBlazor.Infrastructure
 {
