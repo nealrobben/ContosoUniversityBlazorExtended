806418224099b4f84b6435801e1a7f882b388d4a
Neal Robben <neal.robben@gmail.com>
2021-11-30 00:32:38 +0100

    Refactored StudentCreate to remove viewmodel

--- ContosoUniversityBlazorExtended/WebUI/Client/Pages/Students/StudentCreate.razor.cs
+++ ContosoUniversityBlazorExtended/WebUI/Client/Pages/Students/StudentCreate.razor.cs
@@ -1,21 +1,68 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using WebUI.Client.ViewModels.Students;
+using WebUI.Client.Services;
+using WebUI.Shared.Students.Commands.CreateStudent;
 
 namespace WebUI.Client.Pages.Students
 {
     public partial class StudentCreate
     {
         [Inject]
-        public StudentCreateViewModel StudentCreateViewModel { get; set; }
+        public FileuploadService _fileuploadService { get; set; }
+
+        [Inject]
+        public IStudentService StudentService { get; set; }
 
         [CascadingParameter]
         MudDialogInstance MudDialog { get; set; }
 
-        protected override async Task OnInitializedAsync()
+        public CreateStudentCommand CreateStudentCommand { get; set; } = new CreateStudentCommand { EnrollmentDate = DateTime.Now };
+
+        public bool ErrorVisible { get; set; }
+
+        public IList<IBrowserFile> files { get; set; } = new List<IBrowserFile>();
+
+        public async Task FormSubmitted(EditContext editContext)
         {
-            await StudentCreateViewModel.OnInitializedAsync(MudDialog);
+            bool formIsValid = editContext.Validate();
+
+            if (formIsValid)
+            {
+                if (files.Any())
+                {
+                    CreateStudentCommand.ProfilePictureName = await _fileuploadService.UploadFile(files.First());
+                }
+
+                var result = await StudentService.CreateAsync(CreateStudentCommand);
+
+                if (result.IsSuccessStatusCode)
+                {
+                    CreateStudentCommand = new CreateStudentCommand();
+                    MudDialog.Close(DialogResult.Ok(true));
+                }
+                else
+                {
+                    ErrorVisible = true;
+                }
+            }
+        }
+
+        public void Cancel()
+        {
+            MudDialog.Cancel();
+        }
+
+        public void UploadFiles(InputFileChangeEventArgs e)
+        {
+            foreach (var file in e.GetMultipleFiles())
+            {
+                files.Add(file);
+            }
         }
     }
 }
--- ContosoUniversityBlazorExtended/WebUI/Client/Program.cs
+++ ContosoUniversityBlazorExtended/WebUI/Client/Program.cs
@@ -53,7 +53,6 @@ namespace WebUI.Client
             builder.Services.AddTransient<InstructorsViewModel>();
             builder.Services.AddTransient<InstructorEditViewModel>();
 
-            builder.Services.AddTransient<StudentCreateViewModel>();
             builder.Services.AddTransient<StudentsViewModel>();
             builder.Services.AddTransient<StudentEditViewModel>();
         }
