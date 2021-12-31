4c84c2fd00b6a51a6b171b388047bf003ab3057e
Neal Robben <neal.robben@gmail.com>
2021-12-31 14:04:32 +0100

    Added unit tests for Instructors overview

--- ContosoUniversityBlazorExtended/WebUI.Client.Test/Pages/Instructors/InstructorsTests.cs
+++ ContosoUniversityBlazorExtended/WebUI.Client.Test/Pages/Instructors/InstructorsTests.cs
@@ -39,5 +39,47 @@ namespace WebUI.Client.Test.Pages.Instructors
 
             Assert.NotEmpty(dialog.Markup.Trim());
         }
+
+        [Fact]
+        public void Instructors_ClickSearch_CallsInstructorService()
+        {
+            var fakeInstructorService = A.Fake<IInstructorService>();
+            Context.Services.AddScoped(x => fakeInstructorService);
+
+            var fakeCourseService = A.Fake<ICourseService>();
+            Context.Services.AddScoped(x => fakeCourseService);
+
+            var fakeStudentService = A.Fake<IStudentService>();
+            Context.Services.AddScoped(x => fakeStudentService);
+
+            var comp = Context.RenderComponent<Client.Pages.Instructors.Instructors>();
+            Assert.NotEmpty(comp.Markup.Trim());
+
+            comp.Find("#SearchButton").Should().NotBeNull();
+            comp.Find("#SearchButton").Click();
+
+            A.CallTo(() => fakeInstructorService.GetAllAsync(A<string>.Ignored, A<int?>.Ignored, A<string>.Ignored, A<int?>.Ignored)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void Instructors_ClickBackToFullList_CallsInstructorService()
+        {
+            var fakeInstructorService = A.Fake<IInstructorService>();
+            Context.Services.AddScoped(x => fakeInstructorService);
+
+            var fakeCourseService = A.Fake<ICourseService>();
+            Context.Services.AddScoped(x => fakeCourseService);
+
+            var fakeStudentService = A.Fake<IStudentService>();
+            Context.Services.AddScoped(x => fakeStudentService);
+
+            var comp = Context.RenderComponent<Client.Pages.Instructors.Instructors>();
+            Assert.NotEmpty(comp.Markup.Trim());
+
+            comp.Find("#SearchButton").Should().NotBeNull();
+            comp.Find("#SearchButton").Click();
+
+            A.CallTo(() => fakeInstructorService.GetAllAsync(A<string>.Ignored, A<int?>.Ignored, A<string>.Ignored, A<int?>.Ignored)).MustHaveHappened();
+        }
     }
 }
