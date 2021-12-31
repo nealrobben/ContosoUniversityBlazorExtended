3d31f139241950e69ee8d0a1922f4b4fae53dc93
Neal Robben <neal.robben@gmail.com>
2021-12-31 14:16:46 +0100

    Added unit tests for Student overview

--- ContosoUniversityBlazorExtended/WebUI.Client.Test/Pages/Students/StudentTests.cs
+++ ContosoUniversityBlazorExtended/WebUI.Client.Test/Pages/Students/StudentTests.cs
@@ -12,7 +12,7 @@ namespace WebUI.Client.Test.Pages.Students
     public class StudentTests : BunitTestBase
     {
         [Fact]
-        public async Task Students_ClickCreateButton_OpensDialog()
+        public void Students_ClickCreateButton_OpensDialog()
         {
             var fakeStudentService = A.Fake<IStudentService>();
             Context.Services.AddScoped(x => fakeStudentService);
@@ -32,6 +32,36 @@ namespace WebUI.Client.Test.Pages.Students
             comp.Find("#CreateButton").Click();
 
             Assert.NotEmpty(dialog.Markup.Trim());
+        }
+
+        [Fact]
+        public void Students_ClickSearch_CallsStudentService()
+        {
+            var fakeStudentService = A.Fake<IStudentService>();
+            Context.Services.AddScoped(x => fakeStudentService);
+
+            var comp = Context.RenderComponent<Client.Pages.Students.Students>();
+            Assert.NotEmpty(comp.Markup.Trim());
+
+            comp.Find("#SearchButton").Should().NotBeNull();
+            comp.Find("#SearchButton").Click();
+
+            A.CallTo(() => fakeStudentService.GetAllAsync(A<string>.Ignored, A<int?>.Ignored, A<string>.Ignored, A<int?>.Ignored)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void Students_ClickBackToFullList_CallsStudentService()
+        {
+            var fakeStudentService = A.Fake<IStudentService>();
+            Context.Services.AddScoped(x => fakeStudentService);
+
+            var comp = Context.RenderComponent<Client.Pages.Students.Students>();
+            Assert.NotEmpty(comp.Markup.Trim());
+
+            comp.Find("#BackToFullListButton").Should().NotBeNull();
+            comp.Find("#BackToFullListButton").Click();
+
+            A.CallTo(() => fakeStudentService.GetAllAsync(A<string>.Ignored, A<int?>.Ignored, A<string>.Ignored, A<int?>.Ignored)).MustHaveHappened();
         }
     }
 }
