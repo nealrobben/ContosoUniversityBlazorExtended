4e633798c671ff06a1d2243e14ec3667b9082641
Neal Robben <neal.robben@gmail.com>
2021-12-31 01:00:10 +0100

    Added unit test for Departments overview

--- ContosoUniversityBlazorExtended/WebUI.Client.Test/Pages/Departments/DepartmentsTests.cs
+++ ContosoUniversityBlazorExtended/WebUI.Client.Test/Pages/Departments/DepartmentsTests.cs
@@ -1,31 +1,51 @@
 ﻿using Bunit;
 using FakeItEasy;
+using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Localization;
 using MudBlazor;
 using WebUI.Client.Services;
+using WebUI.Client.Test.Extensions;
+using WebUI.Shared.Instructors.Queries.GetInstructorsLookup;
 using Xunit;
 
 namespace WebUI.Client.Test.Pages.Departments
 {
-    public class DepartmentsTests
+    public class DepartmentsTests : BunitTestBase
     {
-        //[Fact]
-        //public void Departments()
-        //{
-        //    using var ctx = new TestContext();
+        [Fact]
+        public async Task Departments_ClickCreateButton_OpensDialog()
+        {
+            var fakeDepartmentService = A.Fake<IDepartmentService>();
+            Context.Services.AddScoped(x => fakeDepartmentService);
 
-        //    var fakeDepartmentService = A.Fake<IDepartmentService>();
+            var instructorsLookupVM = new InstructorsLookupVM
+            {
+                Instructors =
+                {
+                    new InstructorLookupVM
+                    {
+                        ID = 1,
+                        FullName = "Instructor x"
+                    }
+                }
+            };
 
-        //    ctx.Services.AddScoped<IDepartmentService>(x => fakeDepartmentService);
-        //    ctx.Services.AddScoped<IStringLocalizer<Client.Pages.Departments.Departments>>(x => A.Fake<IStringLocalizer<Client.Pages.Departments.Departments>>());
-        //    ctx.Services.AddScoped<IDialogService>(x => A.Fake<IDialogService>());
-        //    ctx.Services.AddScoped<ISnackbar>(x => A.Fake<ISnackbar>());
-        //    //ctx.Services.AddScoped<IDialogService>(x => new DialogService());
+            var fakeInstructorService = A.Fake<IInstructorService>();
+            A.CallTo(() => fakeInstructorService.GetLookupAsync()).Returns(instructorsLookupVM);
+            Context.Services.AddScoped(x => fakeInstructorService);
 
-        //    var cut = ctx.RenderComponent<Client.Pages.Departments.Departments>();
+            var dialog = Context.RenderComponent<MudDialogProvider>();
+            Assert.Empty(dialog.Markup.Trim());
 
-        //    Assert.NotNull(cut);
-        //}
+            var comp = Context.RenderComponent<Client.Pages.Departments.Departments>();
+            Assert.NotEmpty(comp.Markup.Trim());
+
+            comp.Find("h2").TrimmedText().Should().Be("Departments");
+            comp.Find("#CreateButton").Should().NotBeNull();
+
+            comp.Find("#CreateButton").Click();
+
+            Assert.NotEmpty(dialog.Markup.Trim());
+        }
     }
 }
