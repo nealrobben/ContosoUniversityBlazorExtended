61fda6ccbcc177c5de0489427f526b2d789b596d
Neal Robben <neal.robben@gmail.com>
2022-06-06 18:01:20 +0200

    Added UpdateDepartmentValidator

---
+++ ContosoUniversityBlazorExtended/Application/Departments/Validators/UpdateDepartmentValidator.cs
@@ -0,0 +1,29 @@
+﻿using ContosoUniversityBlazor.Application.Common.Interfaces;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+using WebUI.Shared.Departments.Commands.UpdateDepartment;
+
+namespace Application.Departments.Validators
+{
+    public class UpdateDepartmentValidator : WebUI.Shared.Departments.Validators.UpdateDepartmentValidator
+    {
+        private readonly ISchoolContext _context;
+
+        public UpdateDepartmentValidator(ISchoolContext context) : base()
+        {
+            _context = context;
+
+            RuleFor(v => v.Name)
+                .MustAsync(BeUniqueName)
+                    .WithMessage("'Name' must be unique.");
+        }
+
+        public async Task<bool> BeUniqueName(UpdateDepartmentCommand updateDepartment, string name, CancellationToken cancellationToken)
+        {
+            return await _context.Departments
+                .AllAsync(x => !x.Name.Equals(name) && x.DepartmentID != updateDepartment.DepartmentID, cancellationToken);
+        }
+    }
+}
