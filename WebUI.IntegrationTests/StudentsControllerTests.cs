977e517945d38e3c512193d5c0456a613675db92
Neal Robben <neal.robben@gmail.com>
2022-08-15 14:53:18 +0200

    Adding integration tests

--- WebUI.IntegrationTests/StudentsControllerTests.cs
+++ WebUI.IntegrationTests/StudentsControllerTests.cs
@@ -53,6 +53,221 @@ namespace WebUI.IntegrationTests
         }
 
         [Fact]
+        public async Task GetAll_WithSearchString_ReturnsStudentsWithNameMatchingSearchString()
+        {
+            var student1 = new Student
+            {
+                ID = 1,
+                FirstMidName = "ab",
+                LastName = "c",
+                EnrollmentDate = DateTime.UtcNow
+            };
+
+            var student2 = new Student
+            {
+                ID = 2,
+                FirstMidName = "de",
+                LastName = "f",
+                EnrollmentDate = DateTime.UtcNow
+            };
+
+            using (var scope = _appFactory.Services.CreateScope())
+            {
+                var schoolContext = scope.ServiceProvider.GetRequiredService<ISchoolContext>();
+
+                schoolContext.Students.Add(student1);
+                schoolContext.Students.Add(student2);
+                await schoolContext.SaveChangesAsync();
+            }
+
+            var response = await _client.GetAsync("/api/students?searchString=de");
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            var result = (await response.Content.ReadAsAsync<StudentsOverviewVM>());
+
+            result.Students.Should().ContainSingle();
+
+            result.Students.First().StudentID.Should().Be(student2.ID);
+            result.Students.First().FirstName.Should().Be(student2.FirstMidName);
+            result.Students.First().LastName.Should().Be(student2.LastName);
+            result.Students.First().EnrollmentDate.Should().Be(student2.EnrollmentDate);
+        }
+
+        [Fact]
+        public async Task GetAll_WithOrder_ReturnsStudentsInCorrectOrder()
+        {
+            var student1 = new Student
+            {
+                ID = 1,
+                FirstMidName = "ab",
+                LastName = "c",
+                EnrollmentDate = DateTime.UtcNow
+            };
+
+            var student2 = new Student
+            {
+                ID = 2,
+                FirstMidName = "de",
+                LastName = "f",
+                EnrollmentDate = DateTime.UtcNow
+            };
+
+            using (var scope = _appFactory.Services.CreateScope())
+            {
+                var schoolContext = scope.ServiceProvider.GetRequiredService<ISchoolContext>();
+
+                schoolContext.Students.Add(student1);
+                schoolContext.Students.Add(student2);
+                await schoolContext.SaveChangesAsync();
+            }
+
+            var response = await _client.GetAsync("/api/students?sortOrder=lastname_desc");
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            var result = (await response.Content.ReadAsAsync<StudentsOverviewVM>());
+
+            result.Students.Count.Should().Be(2);
+
+            result.Students[0].StudentID.Should().Be(student2.ID);
+            result.Students[0].FirstName.Should().Be(student2.FirstMidName);
+            result.Students[0].LastName.Should().Be(student2.LastName);
+            result.Students[0].EnrollmentDate.Should().Be(student2.EnrollmentDate);
+
+            result.Students[1].StudentID.Should().Be(student1.ID);
+            result.Students[1].FirstName.Should().Be(student1.FirstMidName);
+            result.Students[1].LastName.Should().Be(student1.LastName);
+            result.Students[1].EnrollmentDate.Should().Be(student1.EnrollmentDate);
+        }
+
+        [Fact]
+        public async Task GetAll_WithPageSize_ReturnsOnlyStudentsOnFirstPage()
+        {
+            var student1 = new Student
+            {
+                ID = 1,
+                FirstMidName = "ab",
+                LastName = "c",
+                EnrollmentDate = DateTime.UtcNow
+            };
+
+            var student2 = new Student
+            {
+                ID = 2,
+                FirstMidName = "de",
+                LastName = "f",
+                EnrollmentDate = DateTime.UtcNow
+            };
+
+            var student3 = new Student
+            {
+                ID = 3,
+                FirstMidName = "gh",
+                LastName = "i",
+                EnrollmentDate = DateTime.UtcNow
+            };
+
+            var student4 = new Student
+            {
+                ID = 4,
+                FirstMidName = "jk",
+                LastName = "l",
+                EnrollmentDate = DateTime.UtcNow
+            };
+
+            using (var scope = _appFactory.Services.CreateScope())
+            {
+                var schoolContext = scope.ServiceProvider.GetRequiredService<ISchoolContext>();
+
+                schoolContext.Students.Add(student1);
+                schoolContext.Students.Add(student2);
+                schoolContext.Students.Add(student3);
+                schoolContext.Students.Add(student4);
+                await schoolContext.SaveChangesAsync();
+            }
+
+            var response = await _client.GetAsync("/api/students?pageSize=2");
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            var result = (await response.Content.ReadAsAsync<StudentsOverviewVM>());
+
+            result.Students.Count.Should().Be(2);
+
+            result.Students[0].StudentID.Should().Be(student1.ID);
+            result.Students[0].FirstName.Should().Be(student1.FirstMidName);
+            result.Students[0].LastName.Should().Be(student1.LastName);
+            result.Students[0].EnrollmentDate.Should().Be(student1.EnrollmentDate);
+
+            result.Students[1].StudentID.Should().Be(student2.ID);
+            result.Students[1].FirstName.Should().Be(student2.FirstMidName);
+            result.Students[1].LastName.Should().Be(student2.LastName);
+            result.Students[1].EnrollmentDate.Should().Be(student2.EnrollmentDate);
+        }
+
+        [Fact]
+        public async Task GetAll_WithPageSizeAndPageNumber_ReturnsOnlyStudentsOnSecondPage()
+        {
+            var student1 = new Student
+            {
+                ID = 1,
+                FirstMidName = "ab",
+                LastName = "c",
+                EnrollmentDate = DateTime.UtcNow
+            };
+
+            var student2 = new Student
+            {
+                ID = 2,
+                FirstMidName = "de",
+                LastName = "f",
+                EnrollmentDate = DateTime.UtcNow
+            };
+
+            var student3 = new Student
+            {
+                ID = 3,
+                FirstMidName = "gh",
+                LastName = "i",
+                EnrollmentDate = DateTime.UtcNow
+            };
+
+            var student4 = new Student
+            {
+                ID = 4,
+                FirstMidName = "jk",
+                LastName = "l",
+                EnrollmentDate = DateTime.UtcNow
+            };
+
+            using (var scope = _appFactory.Services.CreateScope())
+            {
+                var schoolContext = scope.ServiceProvider.GetRequiredService<ISchoolContext>();
+
+                schoolContext.Students.Add(student1);
+                schoolContext.Students.Add(student2);
+                schoolContext.Students.Add(student3);
+                schoolContext.Students.Add(student4);
+                await schoolContext.SaveChangesAsync();
+            }
+
+            var response = await _client.GetAsync("/api/students?pageSize=2&pageNumber=1");
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            var result = (await response.Content.ReadAsAsync<StudentsOverviewVM>());
+
+            result.Students.Count.Should().Be(2);
+
+            result.Students[0].StudentID.Should().Be(student3.ID);
+            result.Students[0].FirstName.Should().Be(student3.FirstMidName);
+            result.Students[0].LastName.Should().Be(student3.LastName);
+            result.Students[0].EnrollmentDate.Should().Be(student3.EnrollmentDate);
+
+            result.Students[1].StudentID.Should().Be(student4.ID);
+            result.Students[1].FirstName.Should().Be(student4.FirstMidName);
+            result.Students[1].LastName.Should().Be(student4.LastName);
+            result.Students[1].EnrollmentDate.Should().Be(student4.EnrollmentDate);
+        }
+
+        [Fact]
         public async Task GetSingle_WithNonExistingId_ReturnsNotFound()
         {
             var response = await _client.GetAsync("/api/students/1");
