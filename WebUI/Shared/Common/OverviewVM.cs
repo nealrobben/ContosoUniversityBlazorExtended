256bd5178892a345abb6f908f1d162ee3fe8dd49
Neal Robben <neal.robben@gmail.com>
2022-08-17 21:49:05 +0200

    Added class for overview

---
+++ WebUI/Shared/Common/OverviewVM.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace WebUI.Shared.Common
+{
+    public class OverviewVM<T>
+    {
+        public IList<T> Records { get; set; }
+
+        public MetaData MetaData { get; set; }
+
+        public OverviewVM()
+        {
+            Records = new List<T>();
+            MetaData = new MetaData();
+        }
+
+        public OverviewVM(IList<T> records, MetaData metaData)
+        {
+            if (records != null)
+                Records = records;
+            else
+                Records = new List<T>();
+
+            MetaData = metaData;
+        }
+    }
+}
